5eb1e295bfe8df0c125531baf63a1919fa24f294
chahyeon min <chahyeonmin91@gmail.com>
2025-07-20 23:47:33 +0900

    test

--- src/main/java/hello/itemservice/domain/item/test.java
+++ src/main/java/hello/itemservice/domain/item/test.java
@@ -1,4 +1,10 @@
 package hello.itemservice.domain.item;
 
+import lombok.extern.slf4j.Slf4j;
+
+
 public class test {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
 }
